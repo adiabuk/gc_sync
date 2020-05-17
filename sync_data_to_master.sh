7b049df8f176ac41f7ec2420a605448f96aa394e
Amro Diab <adiab@hotmail.co.uk>
2020-05-17 16:40:34 +0100

    fix syncing script

--- sync_data_to_master.sh
+++ sync_data_to_master.sh
@@ -2,7 +2,7 @@
 
 
 for i in 192.168.0.26 192.168.0.19 192.168.0.15; do
-  rsync -rvhauz --dry-run --exclude=*.log  $i:/data/output ~/data/output
+  rsync -rvhauzt --exclude=*.log  $i:/data/output/* ~/data/output
 
 done
 
